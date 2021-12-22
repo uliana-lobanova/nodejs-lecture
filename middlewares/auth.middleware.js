4e2869035719e54b7bb6f4ef7895dfdef7566287
Uliana Lobanova <uliana.lobanova@binary-studio.com>
2021-12-22 12:19:46 +0200

    add nodejs basics example

--- middlewares/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-const isAuthorized = (req, res, next) => {
-  if (req.headers.authorization === process.env.SECRET_KEY) {
-    next();
-  } else {
-    res.status(401).send();
-  }
-};
-
-module.exports = { isAuthorized };
